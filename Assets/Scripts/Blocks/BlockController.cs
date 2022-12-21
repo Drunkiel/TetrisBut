8f94244d54595f2aeea716773d6fb6b35c34b9c2
Drunkiel <drunkielfel@gmail.com>
2022-12-21 16:24:02 +0100

    Added block controller

---
+++ Assets/Scripts/Blocks/BlockController.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class BlockController : MonoBehaviour
+{
+    public void SetBlocksToBoard()
+    {
+        Transform board = GameObject.FindGameObjectWithTag("Board").transform.GetChild(4).transform;
+        Transform[] blocks = new Transform[transform.childCount];
+
+        for (int i = 0; i < blocks.Length; i++)
+        {
+            blocks[i] = transform.GetChild(i).GetComponent<Transform>();
+        }
+
+        for (int i = 0; i < blocks.Length; i++)
+        {
+            if (blocks[i].name != "Center") blocks[i].parent = board;
+        }
+    }
+
+    public void AutoDestroy()
+    {
+        //Destroying parts to avoid bugs
+        if (TryGetComponent<BlockRotation>(out BlockRotation _blockRotation)) Destroy(GetComponent<BlockRotation>());
+        else if (TryGetComponent<NonStandardBlockRotation>(out NonStandardBlockRotation _nonStandardBlockRotation)) Destroy(GetComponent<NonStandardBlockRotation>());
+        Destroy(GetComponent<BlockMovement>());
+
+        //Spawning next block
+        BoardController board = GameObject.FindGameObjectWithTag("Board").GetComponent<BoardController>();
+        board.spawnBlock = true;
+
+        //Final destroy
+        Destroy(gameObject);
+    }
+}
