2848c1d73cc7b77c0af15cec851b154efb3a785b
Drunkiel <drunkielfel@gmail.com>
2022-12-22 11:37:36 +0100

    Added checking if wall is nearby

--- Assets/Scripts/Blocks/BlockMovement.cs
+++ Assets/Scripts/Blocks/BlockMovement.cs
@@ -8,12 +8,14 @@ public class BlockMovement : MonoBehaviour
     private Transform player;
 
     public CheckCollisions[] _checkCollisions;
+    BlockController _blockController;
 
     // Start is called before the first frame update
     void Start()
     {
         resCooldown = cooldown;
         player = GameObject.FindGameObjectWithTag("Player").transform;
+        _blockController = GetComponent<BlockController>();
     }
 
     // Update is called once per frame
@@ -26,9 +28,24 @@ public class BlockMovement : MonoBehaviour
         }
         else
         {
-            if(cooldown > 1) Movement();
+            if (cooldown > 2) Movement();
             cooldown -= Time.deltaTime;
         }
+    }
+
+    bool CheckIfWall(bool left, bool right)
+    {
+        bool[] ifTrue = new bool[_checkCollisions.Length];
+
+        for (int i = 0; i < ifTrue.Length; i++)
+        {
+            if (left) ifTrue[i] = _checkCollisions[i].CheckWallLeftCollision();
+            if (right) ifTrue[i] = _checkCollisions[i].CheckWallRightCollision();
+
+            if (ifTrue[i]) return true;
+        }
+
+        return false;
     }
 
     void CalculateDistanceToFall()
@@ -38,7 +55,7 @@ public class BlockMovement : MonoBehaviour
 
         for (int i = 0; i < distances.Length; i++)
         {
-            distances[i] = _checkCollisions[i].CheckCollision();
+            distances[i] = _checkCollisions[i].CheckGroundCollision();
         }
 
         int zeroAppearance = 0;
@@ -47,25 +64,33 @@ public class BlockMovement : MonoBehaviour
             if (Mathf.Floor(distances[i]) == 0)
             {
                 zeroAppearance++;
-                if (zeroAppearance >= 1) Destroy(GetComponent<BlockMovement>());
+                if (zeroAppearance >= 1) _blockController.AutoDestroy();
             }
             else if (Mathf.Floor(distances[i]) < nearestBlock && Mathf.Floor(distances[i]) != 0) nearestBlock = distances[i];
         }
 
-        transform.Translate(0, -1.058f * Mathf.Floor(nearestBlock - 1), 0);
+        transform.Translate(0, -1.058f * Mathf.Floor(nearestBlock), 0);
+
+        _blockController.SetBlocksToBoard();
+        _blockController.AutoDestroy();
     }
 
     void Movement()
     {
         float blockDifference = 1.056f;
-        float playerX = Mathf.Floor(player.position.x - GetComponent<BlockRotation>().center.position.x);
+        float playerX = 0;
+
+        if (TryGetComponent<BlockRotation>(out BlockRotation _blockRotation)) playerX = Mathf.Floor(player.position.x - _blockRotation.center.position.x);
+        else if (TryGetComponent<NonStandardBlockRotation>(out NonStandardBlockRotation _nonStandardBlockRotation)) playerX = Mathf.Floor(player.position.x - _nonStandardBlockRotation.center.position.x);
 
         if (playerX >= Mathf.Floor(blockDifference))
         {
+            if (CheckIfWall(false, true)) return;
             directionToMove = 1;
         }
-        else if (playerX < Mathf.Floor(-blockDifference/2) && playerX != 0)
+        else if (playerX < Mathf.Floor(-blockDifference / 2) && playerX != 0)
         {
+            if (CheckIfWall(true, false)) return;
             directionToMove = -1;
         }
         else directionToMove = 0;
