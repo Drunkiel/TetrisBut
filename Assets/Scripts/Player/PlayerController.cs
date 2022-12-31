0b3b1016f57c735eab4cc2b70804e332f403ef0f
Drunkiel <drunkielfel@gmail.com>
2022-12-31 21:59:12 +0100

    Added run animation

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,28 +5,33 @@ public class PlayerController : MonoBehaviour
     public float speedForce;
     public float jumpForce;
 
+    public float radius;
     public bool onTheGround;
     public Transform groundTester;
     public LayerMask layerMask;
 
     Rigidbody rgBody;
+    Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         rgBody = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Collider[] colliders = Physics.OverlapSphere(groundTester.position, 0.51f, layerMask);
+        Collider[] colliders = Physics.OverlapSphere(groundTester.position, radius, layerMask);
 
         if (colliders.Length > 0) onTheGround = true;
         else onTheGround = false;
 
         Movement();
         Jump();
+
+        RotatePlayer();
     }
 
     void Movement()
@@ -35,7 +40,13 @@ public class PlayerController : MonoBehaviour
         float x = Input.GetAxis("Horizontal");
 
         //Player movement
-        if(onTheGround) rgBody.velocity = new Vector2(speedForce * x, rgBody.velocity.y);
+        if(onTheGround)
+        {
+            rgBody.velocity = new Vector2(speedForce * x, rgBody.velocity.y);
+
+            if (x > 0 || x < 0) anim.Play("Run");
+            else anim.Play("Nothing");
+        }
     }
 
     void Jump()
@@ -44,5 +55,10 @@ public class PlayerController : MonoBehaviour
         {
             rgBody.AddForce(new Vector2(0f, jumpForce));
         }
+    }
+
+    void RotatePlayer()
+    {
+        transform.Rotate(0, 180, 0);
     }
 }
