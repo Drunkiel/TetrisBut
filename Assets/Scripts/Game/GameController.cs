5dee1a0d98087fb39a304b3520753e0327e47914
Drunkiel <drunkielfel@gmail.com>
2023-01-03 16:23:43 +0100

    Added game controller

---
+++ Assets/Scripts/Game/GameController.cs
@@ -0,0 +1,51 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public static bool isGameRunning;
+
+    public TMP_Text title;
+
+    public TMP_Text finalScore;
+    public TMP_Text finalHeight;
+    public TMP_Text finalLifeTime;
+
+    public GameObject afterGameScreen;
+
+    public void RunGame()
+    {
+        isGameRunning = true;
+        GameObject.FindGameObjectWithTag("Board").GetComponent<BoardController>().spawnBlock = true;
+    }
+
+    public void StopGame(bool gameOver)
+    {
+        isGameRunning = false;
+
+        if(!gameOver)
+        {
+            title.text = "Winner";
+            title.color = new Color32(0, 255, 0, 1);
+        }
+        else
+        {
+            title.text = "Gameover";
+            title.color = new Color32(255, 0, 0, 1);
+            PlayerController.isDead = true;
+        }
+
+        ScoreController _scoreController = GetComponent<ScoreController>();
+        finalScore.text = _scoreController.score.text;
+        finalHeight.text = _scoreController.playerHeight.ToString();
+        finalLifeTime.text = _scoreController.lifeTime.text;
+
+        afterGameScreen.SetActive(true);
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
