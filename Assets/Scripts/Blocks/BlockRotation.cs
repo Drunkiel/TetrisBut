df092da8e83b4903cd8c0b0bea6bd3f058893855
Drunkiel <drunkielfel@gmail.com>
2023-01-18 18:00:03 +0100

    Simplified rotating logic

--- Assets/Scripts/Blocks/BlockRotation.cs
+++ Assets/Scripts/Blocks/BlockRotation.cs
@@ -1,12 +1,24 @@
 using System.Collections.Generic;
+using System;
 using UnityEngine;
+
+[Serializable]
+public class FieldsToMove
+{
+    public int step1;
+    public int step2;
+    public int step3;
+    public int step4;
+}
 
 public class BlockRotation : MonoBehaviour
 {
     public Transform center;
     public Transform[] blocksToRotate;
+    public FieldsToMove[] fieldsToMove;
+    public int lenghtToCheck;
     public int[] blockStages;
-    public CheckCollisions[] _checkCollisions;
+    private int step;
     public List<Vector3> stages = new List<Vector3>();
 
     // Start is called before the first frame update
@@ -19,24 +31,15 @@ public class BlockRotation : MonoBehaviour
 
     void SetStages()
     {
-        float blockDifference = 1.056f;
-
-        //Setting stage positions
-        stages.Add(new Vector3(center.localPosition.x, center.localPosition.y + blockDifference, 0));
-        stages.Add(new Vector3(center.localPosition.x + blockDifference, center.localPosition.y + blockDifference, 0));
-        stages.Add(new Vector3(center.localPosition.x + blockDifference, center.localPosition.y, 0));
-        stages.Add(new Vector3(center.localPosition.x + blockDifference, center.localPosition.y - blockDifference, 0));
-        stages.Add(new Vector3(center.localPosition.x, center.localPosition.y - blockDifference, 0));
-        stages.Add(new Vector3(center.localPosition.x - blockDifference, center.localPosition.y - blockDifference, 0));
-        stages.Add(new Vector3(center.localPosition.x - blockDifference, center.localPosition.y, 0));
-        stages.Add(new Vector3(center.localPosition.x - blockDifference, center.localPosition.y + blockDifference, 0));
+        if (TryGetComponent<SetStage3x3>(out SetStage3x3 _setStage3)) _setStage3.SetStages();
+        else if (TryGetComponent<SetStage4x4>(out SetStage4x4 _setStage4)) _setStage4.SetStages();
     }
 
     void CheckStage()
     {
         for (int i = 0; i < blocksToRotate.Length; i++)
         {
-            for (int j = 0; j < 8; j++)
+            for (int j = 0; j < 16; j++)
             {
                 if (blocksToRotate[i].localPosition == stages[j])
                 {
@@ -53,13 +56,47 @@ public class BlockRotation : MonoBehaviour
 
         for (int i = 0; i < blocksToRotate.Length; i++)
         {
-            if(_blockMovement._checkCollisions[i].CheckWallLeftCollision(1)) transform.position = new Vector3(transform.position.x + _blockMovement.blockDifference, transform.position.y, 0);
-            if(_blockMovement._checkCollisions[i].CheckWallRightCollision(1)) transform.position = new Vector3(transform.position.x - _blockMovement.blockDifference, transform.position.y, 0);
+            if (_blockMovement._checkCollisions[i].CheckWallLeftCollision(lenghtToCheck)) transform.position = new Vector3(transform.position.x + _blockMovement.blockDifference, transform.position.y, 0);
+            if (_blockMovement._checkCollisions[i].CheckWallRightCollision(lenghtToCheck)) transform.position = new Vector3(transform.position.x - _blockMovement.blockDifference, transform.position.y, 0);
+        }
 
-            blockStages[i] += 2;
-            if (blockStages[i] > 7) blockStages[i] -= 8;
+        switch (step)
+        {
+            case 0:
+                blockStages[0] = fieldsToMove[0].step1;
+                blockStages[1] = fieldsToMove[0].step2;
+                blockStages[2] = fieldsToMove[0].step3;
+                blockStages[3] = fieldsToMove[0].step4;
+                break;
 
+            case 1:
+                blockStages[0] = fieldsToMove[1].step1;
+                blockStages[1] = fieldsToMove[1].step2;
+                blockStages[2] = fieldsToMove[1].step3;
+                blockStages[3] = fieldsToMove[1].step4;
+                break;
+
+            case 2:
+                blockStages[0] = fieldsToMove[2].step1;
+                blockStages[1] = fieldsToMove[2].step2;
+                blockStages[2] = fieldsToMove[2].step3;
+                blockStages[3] = fieldsToMove[2].step4;
+                break;
+
+            case 3:
+                blockStages[0] = fieldsToMove[3].step1;
+                blockStages[1] = fieldsToMove[3].step2;
+                blockStages[2] = fieldsToMove[3].step3;
+                blockStages[3] = fieldsToMove[3].step4;
+                break;
+        }
+
+        for (int i = 0; i < blocksToRotate.Length; i++)
+        {
             blocksToRotate[i].localPosition = stages[blockStages[i]];
         }
+
+        if (step >= 3) step = 0;
+        else step++;
     }
 }
