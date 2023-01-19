958fc98baabef8110eac62afdf38ddb2f79380b3
Drunkiel <drunkielfel@gmail.com>
2023-01-19 17:00:04 +0100

    Added points controller

--- Assets/Scripts/Game/Board/CheckBlocksRow.cs
+++ Assets/Scripts/Game/Board/CheckBlocksRow.cs
@@ -6,6 +6,7 @@ public class CheckBlocksRow : MonoBehaviour
 {
     public bool check;
     public List<float> blocksHeight = new List<float>();
+    private float lastCheckedLine;
     public float delay;
 
     public int blocksInRow;
@@ -33,6 +34,7 @@ public class CheckBlocksRow : MonoBehaviour
         if (blocksInRow >= 1)
         {
             hittedBlocks = hits;
+            lastCheckedLine = heightToCheck;
             return true;
         }
         else
@@ -52,7 +54,27 @@ public class CheckBlocksRow : MonoBehaviour
             caughtBlocks[i] = hittedBlocks[i].transform.gameObject;
         }
 
-        if (caughtBlocks.Length == 10) print("Full row: " + caughtBlocks[0].transform.position.y);
+        if (caughtBlocks.Length == 10)
+        {
+            //Adding points for getting line
+            ScoreController.AddScore(40);
+            ScoreController.pointsMultiplier += 1;
+            ScoreController.timeBeforeReset += 5 + 3 * MainMenuController.pickedDifficulty;
+
+            //Destroying blocks in line
+            for (int i = 0; i < caughtBlocks.Length; i++)
+            {
+                Destroy(caughtBlocks[i]);
+            }
+
+            //Falling blocks from above
+            GameObject[] restBlocks = GameObject.FindGameObjectsWithTag("Block");
+
+            foreach (GameObject singleBlock in restBlocks)
+            {
+                singleBlock.transform.position = new Vector3(singleBlock.transform.position.x, singleBlock.transform.position.y - 1.056f, singleBlock.transform.position.z);
+            }
+        }
 
         check = false;
     }
@@ -63,7 +85,7 @@ public class CheckBlocksRow : MonoBehaviour
 
         for (int i = 0; i < blocksHeight.Count; i++)
         {
-            if (SendLaser(blocksHeight[i])) CheckBlocks();
+            if (lastCheckedLine != blocksHeight[i] && SendLaser(blocksHeight[i])) CheckBlocks();
         }
     }
 }
